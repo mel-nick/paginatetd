bffaadf2ba14aec2c2f5e694ea8c05bf59a65b42
mel-nick <48454124+mel-nick@users.noreply.github.com>
2020-06-30 13:55:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(
 // headers
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", *); 
+  res.header("Access-Control-Allow-Origin", "*"); 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
